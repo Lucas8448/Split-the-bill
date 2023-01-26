8de9017b7a14ec216e04bfc3ad3e656ff85ee966
Lucas Bateson <lucas.bateson@gmail.com>
2023-01-26 08:29:50 +0100

    Initial Commit

---
+++ Split the bill/Split_the_billApp.swift
@@ -0,0 +1,17 @@
+//
+//  Split_the_billApp.swift
+//  Split the bill
+//
+//  Created by Lucas William Bateson on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Split_the_billApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
